57c74e220356dbe1ce9c7fc189838b8a008f5ff7
moniaparnesh <moniaparnesh@gmail.com>
2026-05-12 08:18:58 +0530

    Update settings.py

--- sample/sample/settings.py
+++ sample/sample/settings.py
@@ -38,7 +38,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sampleapp',
+    'rest_framework',
+
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
